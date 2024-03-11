370bfa71ccaf15ff92b5934bea5cc1008abd65e3
giftthomu <giftthomu@gmail.com>
2024-03-11 14:28:22 +0200

    House Keeping

--- CRUD WEB API/Controllers/UsersController.cs
+++ CRUD WEB API/Controllers/UsersController.cs
@@ -36,6 +36,63 @@ namespace CRUD_WEB_API.Controllers
 
             return user;
         }
-       
+
+        [HttpPost]
+        public async Task<ActionResult<Users>> PostUser(Users user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult<Users>> DeleteUser(int Id)
+        {
+            var user = await _context.Users.FindAsync(Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent(); 
+
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, Users user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+
     }
 }
