6cc7e99e66d9b34009da132b692dc207f3ca50b3
giftthomu <giftthomu@gmail.com>
2024-03-11 10:17:29 +0200

    Added the controller

---
+++ CRUD WEB API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using CRUD_WEB_API.Data;
+using CRUD_WEB_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_WEB_API.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly CrudWebApiContext _context;
+
+       
+        public UsersController(CrudWebApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Users>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+       
+    }
+}
