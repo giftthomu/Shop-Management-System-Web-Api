a9778a4d650e8a8923174e62699bb379397b58a2
giftthomu <giftthomu@gmail.com>
2024-03-11 10:03:21 +0200

    Added DbContext and Model

---
+++ CRUD WEB API/Data/CrudWebApiContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD_WEB_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_WEB_API.Data
+{
+    public class CrudWebApiContext:DbContext
+    {
+        public CrudWebApiContext(DbContextOptions<CrudWebApiContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ CRUD WEB API/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUD_WEB_API.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string email { get; set; }
+        public string userType { get; set; }
+    }
+}
