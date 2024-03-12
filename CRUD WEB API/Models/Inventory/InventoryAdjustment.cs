b84bbd526d016009148e72f05800f2f5e1af918d
giftthomu <giftthomu@gmail.com>
2024-03-12 09:44:08 +0200

    Added Inventory Models

---
+++ CRUD WEB API/Models/Inventory/InventoryAdjustment.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUD_WEB_API.Models.Inventory
+{
+    public class InventoryAdjustment
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int QuantityAdjustment { get; set; }
+        public string Reason { get; set; }
+        public DateTime AdjustmentDate { get; set; }
+    }
+}
---
+++ CRUD WEB API/Models/Inventory/SupplierPurchaseOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUD_WEB_API.Models.Inventory
+{
+    public class SupplierPurchaseOrder
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int SupplierId { get; set; }
+        public List<SupplierPurchaseOrderItem> Items { get; set; }
+        public decimal TotalAmount => Items.Sum(item => item.Price * item.Quantity);
+    }
+}
